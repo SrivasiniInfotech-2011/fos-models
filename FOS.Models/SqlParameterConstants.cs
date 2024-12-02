628aae37cb1edb3c8ea6e07c827685f3de36ef0b
karthikrossi46 <karthik.n@ascentya.in>
2024-12-02 23:11:54 +0530

    User Insert Api changes in repository and Models

--- FOS.Models/SqlParameterConstants.cs
+++ FOS.Models/SqlParameterConstants.cs
@@ -67,5 +67,37 @@
         public const string PROSPECT_CREATED_BY = "@Created_By";
         public const string PROSPECT_CODE = "@Prospect_Code";
         public const string PROSPECT_ERROR_CODE = "@Error_Code";
+
+
+
+
+        //USER MANAGEMENT INSERT
+        public const string USER_MANAGEMENT_COMPANY_ID = "@Company_ID";
+        public const string USER_MANAGEMENT_USER_ID = "@User_ID";
+        public const string USER_MANAGEMENT_USER_CODE = "@UserCode";
+        public const string USER_MANAGEMENT_USER_NAME = "@UserName";
+        public const string USER_MANAGEMENT_GENDER_ID = "@Gender_ID";
+        public const string USER_MANAGEMENT_PASSWORD = "@Password";
+        public const string USER_MANAGEMENT_MOBILE_NUMBER = "@MobileNumber";
+        public const string USER_MANAGEMENT_EMERGENCY_CONTACT_NUMBER = "@EmergencycontactNumber";
+        public const string USER_MANAGEMENT_DOJ = "@DOJ";
+        public const string USER_MANAGEMENT_DESIGNATION = "@Designation";
+        public const string USER_MANAGEMENT_USER_LEVEL_ID = "@UserLevelID";
+        public const string USER_MANAGEMENT_REPORTING_NEXT_LEVEL = "@ReportingNextlevel";
+        public const string USER_MANAGEMENT_USER_GROUP = "@User_Group";
+        public const string USER_MANAGEMENT_EMAIL_ID = "@EmailID";
+        public const string USER_MANAGEMENT_DATEOFBIRTH = "@Dateofbirth";
+        public const string USER_MANAGEMENT_FATHER_NAME = "@FatherName";
+        public const string USER_MANAGEMENT_MOTHER_NAME = "@MotherName";
+        public const string USER_MANAGEMENT_SPOUSE_NAME = "@SpouseName";
+        public const string USER_MANAGEMENT_MARITIAL_ID = "@Maritial_ID";
+        public const string USER_MANAGEMENT_AADHAR_NUMBER = "@Aadhar_Number";
+        public const string USER_MANAGEMENT_PAN_NUMBER = "@PAN_Number";
+        public const string USER_MANAGEMENT_Address = "@Address";
+        public const string USER_MANAGEMENT_USER_IMAGE_PATH = "@User_Imagepath";
+        public const string USER_MANAGEMENT_IS_ACTIVE = "@Is_Active";
+        public const string USER_MANAGEMENT_CREATED_BY = "@CreatedBy";
+        public const string USER_MANAGEMENT_ERROR_CODE = "@ErrorCode";
+
     }
 }
